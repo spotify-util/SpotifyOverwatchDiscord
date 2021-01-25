687b700cd38439f025f0738d6c0f374718a3d6d5
elijaholmos <eli.olmos9@gmail.com>
2021-01-25 08:05:08 -0700

    ping cmd update
    now shows spotify api latency

--- commands/general/ping.js
+++ commands/general/ping.js
@@ -1,11 +1,25 @@
+const { getUserProfile } = require('../../spotify-overwatch.js');
+
 module.exports = {
 	name: 'ping',
 	group: 'general',
-	description: 'Get the latentcy of the bot',
+	description: 'Get the latentcy of the bot and its connected APIs',
 	args: false,
     usage: '',
     aliases: [],
-	execute({bot, message, args}) {
-        return message.channel.send('Pinging...').then((sent) => sent.edit(`Took ${sent.createdTimestamp - message.createdTimestamp}ms to respond. Discord API latency is ${bot.ws.ping}ms`));
+	async execute({bot, message, args}) {
+		const sent_message = await message.channel.send('Pinging...');	//send response
+		const latency = { discord: bot.ws.ping, spotify: new Date() };	//generate latency variables
+		sent_message.edit(`Took ${sent_message.createdTimestamp - message.createdTimestamp}ms to respond.\nDiscord API latency is ${latency.discord}ms\nPinging Spotify API...`
+		);
+		getUserProfile('ollog10').then(() => {
+			latency.spotify = Date.now() - latency.spotify.getTime();
+			sent_message.edit(`Took ${sent_message.createdTimestamp - message.createdTimestamp}ms to respond.\nDiscord API latency is ${latency.discord}ms\nSpotify API latency is ${latency.spotify}ms`
+			);
+		}).catch((err) => {
+			console.log(err);
+			sent_message.edit(`Took ${sent_message.createdTimestamp - message.createdTimestamp}ms to respond.\nDiscord API latency is ${latency.discord}ms\nSpotify API was pinged unsuccessfully`
+			);
+		});
     }
 };
